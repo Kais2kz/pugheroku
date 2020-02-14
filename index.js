a1d4fd2d7b68bd176b74b55dad3d52fc0cd9c5ce
PlayboiCarti <46751826+Kais2kz@users.noreply.github.com>
2020-02-14 13:28:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,40 +19,5 @@ newsapi.v2.topHeadlines({
     }
   */
 });
-// To query /v2/everything
-// You must include at least one q, source, or domain
-newsapi.v2.everything({
-  q: 'bitcoin',
-  sources: 'bbc-news,the-verge',
-  domains: 'bbc.co.uk, techcrunch.com',
-  from: '2020-02-01',
-  to: '2020-02-02',
-  language: 'en',
-  sortBy: 'relevancy',
-  page: 1
-}).then(response => {
-  console.log(response);
-  /*
-    {
-      status: "ok",
-      articles: [...]
-    }
-  */
-});
-// To query sources
-// All options are optional
-newsapi.v2.sources({
-  category: 'technology',
-  language: 'en',
-  country: 'us'
-}).then(response => {
-  console.log(response);
-  /*
-    {
-      status: "ok",
-      sources: [...]
-    }
-  */
-});
 
 app.listen(3000);
