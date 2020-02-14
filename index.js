15f8c9c802e45ca1f0a76695e80182f28c474c71
PlayboiCarti <46751826+Kais2kz@users.noreply.github.com>
2020-02-14 13:24:02 +0100

    publish

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const app = express();
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI('fd47197061764123b91602ea392d3611');
+app.set('view engine', 'pug');
+
+newsapi.v2.topHeadlines({
+  sources: 'bbc-news,the-verge',
+  q: 'bitcoin',
+  category: 'business',
+  language: 'en',
+  country: 'us'
+}).then(response => {
+  console.log(response);
+  /*
+    {
+      status: "ok",
+      articles: [...]
+    }
+  */
+});
+// To query /v2/everything
+// You must include at least one q, source, or domain
+newsapi.v2.everything({
+  q: 'bitcoin',
+  sources: 'bbc-news,the-verge',
+  domains: 'bbc.co.uk, techcrunch.com',
+  from: '2020-02-01',
+  to: '2020-02-02',
+  language: 'en',
+  sortBy: 'relevancy',
+  page: 1
+}).then(response => {
+  console.log(response);
+  /*
+    {
+      status: "ok",
+      articles: [...]
+    }
+  */
+});
+// To query sources
+// All options are optional
+newsapi.v2.sources({
+  category: 'technology',
+  language: 'en',
+  country: 'us'
+}).then(response => {
+  console.log(response);
+  /*
+    {
+      status: "ok",
+      sources: [...]
+    }
+  */
+});
+
+app.listen(3000);
