7fb65448ede242346262e721b42bfee614221962
PlayboiCarti <46751826+Kais2kz@users.noreply.github.com>
2020-02-14 13:43:39 +0100

    yee

--- index.js
+++ index.js
@@ -2,22 +2,21 @@ const express = require('express');
 const app = express();
 const NewsAPI = require('newsapi');
 const newsapi = new NewsAPI('fd47197061764123b91602ea392d3611');
-app.set('view engine', 'pug');
+var port = process.env.PORT || 3000
+app.set('view engine', 'pug')
+
 
 newsapi.v2.topHeadlines({
-  sources: 'bbc-news,the-verge',
-  q: 'bitcoin',
+  sources: '',
+  q: '',
   category: 'business',
   language: 'en',
   country: 'us'
 }).then(response => {
-  console.log(response);
-  /*
-    {
-      status: "ok",
-      articles: [...]
-    }
-  */
+  app.get('/', function(req, res){
+    res.render('index',{title:
+    JSON.stringify(response["totalResults"]), paragraf:
+    JSON.stringify(response["articles"])})
+  });
 });
-
 app.listen(3000);
